77727afd29a91b73a937651a29e610aab4e67b9a
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2021-01-12 17:46:53 +0800

    Getting Started with Go
    Getting Started with Go

--- 1. Getting Started with Go/Week 3 – Composite Data Types/Assignment_1/src/main/slice.go
+++ 1. Getting Started with Go/Week 3 – Composite Data Types/Assignment_1/src/main/slice.go
@@ -11,10 +11,13 @@ func main() {
 	var arr_slice []int = make([]int, 0, 3)
 	var i int
 
-	for i=0; i<3; i++ {
-		fmt.Printf("Input your number: ")
-		fmt.Scan(&input_number) 	
-		
+	fmt.Println("Enter an interger or X to exit: ")
+	for true {
+		fmt.Scanln(&input_number)
+		if input_number == "X"{
+            break
+		}
+
 		input_number, err := strconv.Atoi(input_number)
 		if err != nil{
 			fmt.Println("Your input must be a number")
@@ -22,8 +25,8 @@ func main() {
 			continue
 		} else {
 			arr_slice = append(arr_slice, input_number)
-		}		
-	}	
+		}	
+	}
 	sort.Ints(arr_slice[:])
 	fmt.Println(arr_slice)
 }
